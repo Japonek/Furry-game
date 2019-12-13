93953cb78decd8454659a4411a442a35157a8d73
Igor <igor.klusak@gmail.com>
2019-12-13 18:07:01 +0100

    add new game menu

--- js/app.js
+++ js/app.js
@@ -14,9 +14,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     }
 
+
+
     function Game() {
+        let continueButtons = document.getElementsByClassName("continue-button")
+        let endGame = document.querySelector(".container-endgame")
+        let endGameElements = document.querySelector(".endgame-elements")
+        let newGameElements = document.querySelector(".newgame-elements")
         let board = document.getElementById('board');
-        this.board = board.getElementsByTagName('div');
+        this.board = board.getElementsByClassName("tile");
         this.furry = new Furry();
         this.coin = new Coin();
         this.score = 0;
@@ -24,48 +30,63 @@ document.addEventListener("DOMContentLoaded", function (event) {
         this.index = (x, y) => {
             return (x + (y * 10));
         };
+        this.start = (event) => {
+            document.addEventListener('keydown', function start(event) {
+                if (event.keyCode === 68) {
+                    game.hideVisibleFurry()
+                    game.showFurry();
+                    game.showCoin();
+                    game.startGame();
+                    document.removeEventListener('keydown', start)
+                }
+            })
 
-        this.furry
+        }
         this.showFurry = () => {
-            //  console.log(this.furry.x+" "+this.furry.y)
-            this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+            let furry = document.createElement('div')
+            furry.classList.add("furry")
+            if (this.furry.direction === "left") furry.classList.add("rotate")
+            this.board[this.index(this.furry.x, this.furry.y)].appendChild(furry)
 
         };
         this.showCoin = () => {
-            this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+            let coin = document.createElement('div')
+            coin.classList.add("coin")
+            this.board[this.index(this.coin.x, this.coin.y)].appendChild(coin)
         };
         this.startGame = () => {
-
+            endGame.classList.add("invisible")
             this.control()
             this.idSetInterval = setInterval(() => {
                 this.moveFurry()
             }, this.interval);
-
-            this.moveFurry = () => {
-
-                if (this.furry.direction === "right") {
-                    this.furry.x++;
-
-                } else if (this.furry.direction === "left") {
-                    this.furry.x--;
-
-                } else if (this.furry.direction === "up") {
-                    this.furry.y--;
-
-                } else if (this.furry.direction === "down") {
-                    this.furry.y++;
-
-                }
-                if (this.gameOver()) {
-                    this.hideVisibleFurry();
-                    this.checkCoinCollision();
-                    this.showFurry()
-                }
-            }
-
         };
+        this.moveFurry = () => {
+
+            if (this.furry.direction === "right") {
+                this.furry.x++;
+
+            } else if (this.furry.direction === "left") {
+                this.furry.x--;
+
+            } else if (this.furry.direction === "up") {
+                this.furry.y--;
+
+            } else if (this.furry.direction === "down") {
+                this.furry.y++;
+
+            }
+            if (this.gameOver()) {
+                this.hideVisibleFurry();
+                this.checkCoinCollision();
+                this.showFurry()
+            }
+        }
         this.hideVisibleFurry = () => {
-            document.querySelector(".furry").classList.remove('furry')
+            document.querySelector(".furry").parentNode.removeChild(document.querySelector(".furry"))
+        };
+        this.hideVisibleCoin = () => {
+            document.querySelector(".coin").parentNode.removeChild(document.querySelector(".coin"))
         };
         this.turnFurry = function (event) {
             switch (event.which) {
@@ -86,13 +107,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         this.checkCoinCollision = () => {
             if (this.furry.x === this.coin.x && this.furry.y === this.coin.y) {
-                document.querySelector("div.coin").classList.remove("coin");
+                this.hideVisibleCoin()
                 this.score++;
                 if (this.interval > 60) this.interval -= 40
-                console.log(this.interval)
                 clearInterval(this.idSetInterval);
                 this.startGame()
-                document.getElementById("scoreboard").innerText = (this.score);
+                document.querySelectorAll(".scoreboard").forEach((score) => score.innerText = (this.score))
                 this.coin = new Coin()
                 if (this.coin.x === this.furry.x && this.coin.y === this.furry.y) this.coin.y = 8
                 this.showCoin()
@@ -103,52 +123,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
             if (this.furry.x < 0 || this.furry.x > 9 || this.furry.y < 0 || this.furry.y > 9) {
                 clearInterval(this.idSetInterval);
                 this.hideVisibleFurry()
-                let endGame = document.createElement("div")
-                endGame.classList.add('logScreen')
-                let score = document.getElementById('score')
-
-                let continueGame = document.createElement('button')
-                continueGame.innerText = "Continue"
-                continueGame.classList.add("button")
-                endGame.appendChild(continueGame)
-                let newGame = document.createElement('button')
-                newGame.innerText = "New game"
-                newGame.classList.add("button")
-                endGame.appendChild(newGame)
-                let wizard = document.createElement('div')
-                wizard.classList.add("furry")
-                endGame.appendChild(wizard)
-
-
-
-                document.querySelector(".container").insertBefore(endGame, score)
-
-                continueGame.addEventListener('click', this.continueGame)
-                newGame.addEventListener('click', this.restartGame)
+                this.hideVisibleCoin()
+                endGameElements.classList.remove("invisible")
+                endGame.classList.remove("invisible")
+                newGameElements.style = ("display:none")
+                document.getElementById("score").classList.add("invisible")
+                let furry = document.createElement('div')
+                furry.classList.add("furry")
+                continueButtons[0].appendChild(furry)
+                document.addEventListener('keydown', game.select)
                 return false
             }
             return true
         }
         this.continueGame = () => {
-            endGame = document.querySelector('.logScreen')
-            endGame.parentNode.removeChild(endGame)
-
+            document.removeEventListener('keydown', game.select)
+            endGame.classList.add("invisible")
+            this.hideVisibleFurry()
             this.furry.x = 0
             this.furry.y = 0
             this.furry.direction = "right"
-
+            document.getElementById("score").classList.remove("invisible")
+            this.showCoin()
             this.showFurry()
             game.startGame()
         }
         this.restartGame = () => {
-            endGame = document.querySelector(".logScreen")
-            endGame.parentNode.removeChild(endGame)
-            delete(this.furry)
-            this.furry = new Furry()
+            document.removeEventListener('keydown', game.select)
+            endGame.classList.add("invisible")
+            this.hideVisibleFurry()
+            document.getElementById("score").classList.remove("invisible")
+            this.furry.x = 0
+            this.furry.y = 0
+            this.furry.direction = "right"
             this.score = 0
-            document.getElementById("scoreboard").innerText = (this.score);
+            document.querySelectorAll(".scoreboard").forEach((score) => score.innerText = (this.score))
             this.interval = 500
-            document.querySelector("div.coin").classList.remove("coin");
             delete(this.coin)
             this.coin = new Coin();
             this.showCoin()
@@ -161,14 +171,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 game.turnFurry(event);
             });
         }
+        this.select = (event) => {
+            if (event.keyCode === 87 || event.keyCode === 83) {
+                if (continueButtons[0].contains(document.querySelector(".furry"))) {
+                    this.hideVisibleFurry()
+                    let furry = document.createElement('div')
+                    furry.classList.add("furry")
+                    continueButtons[1].appendChild(furry)
+                } else {
+                    this.hideVisibleFurry()
+                    let furry = document.createElement('div')
+                    furry.classList.add("furry")
+                    continueButtons[0].appendChild(furry)
+                }
+            } else if (event.keyCode === 68) {
+                (continueButtons[0].contains(document.querySelector(".furry"))) ? this.continueGame(): this.restartGame()
+            }
+
+
+        }
 
     }
 
-    game.showFurry();
-    game.showCoin();
-    game.startGame();
+    game.start(event)
 
-
-
-
-});
+})
